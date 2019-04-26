55a700f89fff8300996d6d0a10afea1a344b13f3
Song Shik <42000388+SongShik@users.noreply.github.com>
2019-04-26 18:01:24 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,402 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <title>ICOM seguros</title>
+
+
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.css'>
+
+  <link rel="stylesheet" href="css/style.css">
+
+
+</head>
+
+<body>
+  <!-- menu -->
+  <section>
+    <nav class="navbar navbar-expand-lg menu">
+      <a class="navbar-brand" href="#">
+        <img src="img/icone.png" alt="">
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <img src="img/menu-icone.png" alt="">
+      </button>
+
+
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+          <li class="nav-item"> <a class="nav-link" href="#"> Área do Cliente </a> </li>
+          <li class="nav-item"> <a class="nav-link" href="#quemsomos"> Quem Somos </a> </li>
+          <li class="nav-item"> <a class="nav-link" href="#"> Acesso Adminstrativo </a> </li>
+        </ul>
+      </div>
+    </nav>
+
+  </section>
+
+  <!-- carousel -->
+  <section>
+
+    <div class="imagem-principal  text-center">
+      <div class="overlay">
+        <p class="titulo-slider  margin d-none d-lg-block">Pensou em saúde para sua empresa,</p>
+        <div class="row mt-3">
+          <div class="col">
+            <hr class="linha-icom">
+          </div>
+          <p class="ICOM d-none d-lg-block"> PENSOU ICO<span class="texto-icom">M</span></p>
+          <p class="ICOM  d-lg-none">ICO<span class="texto-icom">M</span></p>
+          <div class="col">
+            <hr class="linha-icom">
+          </div>
+        </div>
+
+        <div class="tamanho-input mt-3 ">
+          <input type="text" class="form-control email text-center"
+            placeholder="Insira seu e-mail para iniciar a cotação">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- vantagens -->
+  <section>
+    <div class="row mt-3">
+      <div class="col">
+        <hr class="linha">
+      </div>
+      <div class="col-auto titulo">Vantagens da ICOM</div>
+      <div class="col">
+        <hr class="linha">
+      </div>
+    </div>
+    
+    <!-- carousel pequeno-->
+    <div id="carouselExampleControls" class="carousel slide d-lg-none" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <div class="box-externo ">
+            <div class="box ">
+              <div class="box-esquerda ">
+                <img src="img/pip.png" class="img-fluid ml-2" alt="">
+              </div>
+              <div class="box-direita texto-cinza  ">
+                <p class="font-weight-bold ">Foco em Redução de Custo</p>
+                O Principal foco da ICOM Seguros é reduzir o custo que a empresa tem com plano de saúde empresarial.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div class="box-externo ">
+            <div class="box ">
+              <div class="box-esquerda ">
+                <img src="img/maos.png" class="img-fluid ml-2" alt="">
+              </div>
+              <div class="box-direita texto-cinza">
+                <p class="font-weight-bold">Ecxelência em pós venda</p>
+                Buscamos sempre estreitar nosso relacionamento com nosso cliente para que ele saiba que poderá sempre
+                contar com a ICOM.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div class="box-externo ">
+            <div class="box ">
+              <div class="box-esquerda ">
+                <img src="img/money.png" class="img-fluid ml-2" alt="">
+              </div>
+              <div class="box-direita texto-cinza ">
+                <p class="font-weight-bold">Desconto ICOM</p>
+                A ICOM também oferece um desconto exclusivo na contratação de planos para PME, cote seu plano e veja a
+                diferença!
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+    <div class="container d-none d-lg-block">
+      <div class="row ">
+        <div class="col-lg-4 col-md-6  mt-4 ">
+          <div class="box ">
+            <div class="box-esquerda ">
+              <img src="img/pip.png" class="img-fluid ml-2" alt="">
+            </div>
+            <div class="box-direita texto-cinza  ">
+              <p class="font-weight-bold ">Foco em Redução de Custo</p>
+              O Principal foco da ICOM Seguros é reduzir o custo que a empresa tem com plano de saúde empresarial.
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6  mt-4 ">
+          <div class="box ">
+            <div class="box-esquerda ">
+              <img src="img/maos.png" class="img-fluid ml-2" alt="">
+            </div>
+            <div class="box-direita texto-cinza">
+              <p class="font-weight-bold">Ecxelência em pós venda</p>
+              Buscamos sempre estreitar nosso relacionamento com nosso cliente para que ele saiba que poderá sempre
+              contar com a ICOM.
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 mt-4 ">
+          <div class="box ">
+            <div class="box-esquerda ">
+              <img src="img/money.png" class="img-fluid ml-2" alt="">
+            </div>
+            <div class="box-direita texto-cinza ">
+              <p class="font-weight-bold">Desconto ICOM</p>
+              A ICOM também oferece um desconto exclusivo na contratação de planos para PME, cote seu plano e veja a
+              diferença!
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!--depoimentos-->
+  <section>
+    <div class="row mt-5">
+      <div class="col">
+        <hr class="linha">
+      </div>
+      <div class="col-auto titulo ">Depoimentos</div>
+      <div class="col">
+        <hr class="linha">
+      </div>
+    </div>
+    <div class="container">
+      <div id="depoimentos-carousel" class="carousel slide text-center" data-ride="carousel">
+        <div class="carousel-inner row">
+          <div class="carousel-item col-md-4 active">
+            <div class="depoimento-centralizar  ">
+              <div class="img-depoimento">
+                <img src="img/depoimento1.jpeg" class="img-fluid rounded-circle" alt="Imagem não carregou" width="150"
+                  height="150">
+              </div>
+              <p class="mt-3 text-center titulo-cinza">Lilian Lemos</p>
+              <p class="text-center sub-titulo ">Estrelacom</p>
+              A qualidade, agilidade e ética no atendimento da ICOM não nos deixou dúvida, e hoje estamos com 100% de
+              nossos benefícios com eles.
+              <div class="img-depoimento text-center">
+                <img src="img/icon.png" class="img-fluid mt-3" alt="Imagem não carregou">
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item col-md-4">
+            <div class="depoimento-centralizar  ">
+              <div class="img-depoimento">
+                <img src="img/depoimento2.jpeg" class="img-depoimento " alt="Imagem não carregou">
+              </div>
+              <p class="mt-3 text-center titulo-cinza">Caroline Yoshida</p>
+              <p class="sub-titulo">Vérios</p>
+              Trabalhar com a ICOM na implantação do benefício saúde da empresa foi fácil e descomplicado, os
+              consultores nos prestaram em excelente atendimento em todos os processos.
+              <div class="img-depoimento text-center">
+                <img src="img/icon.png" class="img-fluid mt-3" alt="Imagem não carregou">
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item col-md-4">
+            <div class="depoimento-centralizar   ">
+              <div class="img-depoimento">
+                <img src="img/depoimento3.jpeg" class="img-depoimento " alt="Imagem não carregou">
+              </div>
+              <p class="mt-3 text-center titulo-cinza">Carina</p>
+              <p class="text-center sub-titulo">G&P</p>
+              A ICOM é uma corretora comprometida e sempre com disponibilidade em nos ajudar. Nunca ficamos sem
+              atendimento e sempre somos acompanhados até a resolução. É um parceiro com qualidade ímpar.
+              <div class="img-depoimento text-center ">
+                <img src="img/icon.png" class="img-fluid mt-3 " alt="Imagem não carregou">
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item col-md-4">
+            <div class="depoimento-centralizar   ">
+              <div class="img-depoimento">
+                <img src="img/depoimento4.jpeg" class="img-depoimento " alt="Imagem não carregou">
+              </div>
+              <p class="mt-3 text-center titulo-cinza">Claudia Pieri</p>
+              <p class="text-center sub-titulo">Gefran</p>
+              Os profissionais da ICOM são proativos, tem iniciativa e são extremamente competentes.
+              A Gefran gostaria de agradecer a parceria e espera contar com a ICOM por um longo tempo
+              <div class="img-depoimento text-center ">
+                <img src="img/icon.png" class="img-fluid mt-3 " alt="Imagem não carregou">
+              </div>
+            </div>
+          </div>
+        </div>
+        <a class="carousel-control-prev " href="#depoimentos-carousel" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next " href="#depoimentos-carousel" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
+  </section>
+
+  <!--quem somos-->
+  <section id="quemsomos">
+    <div class="row mt-5">
+      <div class="col">
+        <hr class="linha">
+      </div>
+      <div class="col-auto titulo">Quem Somos</div>
+      <div class="col">
+        <hr class="linha">
+      </div>
+    </div>
+
+
+    <div class="container mt-4">
+      <div class="row ">
+        <div class="col-md-6  ">
+          <div class="logo-icon">
+            <img class="img-fluid" src="img/ICOM LOGO.png" alt="Imagem não carregou">
+          </div>
+        </div>
+        <div class="col-md-6 text-justify "><br>
+          <p>Somos uma consultoria em seguros B2B especializada na área de benefícios
+            (plano de saúde, odontológico e seguro de vida) dos funcionários.</p>
+          <p> Trabalhamos com todas as seguradoras do mercado para assim identificar
+            o melhor custo benefício para atender de forma personalizada cada empresa. </p>
+          Entendemos que o plano de saúde não precisa ter um custo altíssimo para ser eficiente
+          e atender as necessidades da empresa e do beneficiário.
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!--contato-->
+  <section>
+    <div class="container mt-4">
+      <div class="row ">
+        <div class="col-md-6  ">
+          <div class="row map-responsive border-sombra">
+            <!--
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d29257.802950452795!2d-46.63654300000001!3d-23.560348!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb6a53ce5ab0879b4!2sICOM+seguros!5e0!3m2!1spt-BR!2sbr!4v1555515730974!5m2!1spt-BR!2sbr"
+            frameborder="0" style="border:0" allowfullscreen></iframe>
+-->
+          </div>
+          <div class="row mt-5 text-center ">
+            <div class="col ">
+              <div class="imagem-localizacao"> <img src="img/localizacao.png" class="img-fluid "
+                  alt="Imagem não carregou"></div>
+              <p class="mt-3">R. José Getúlio, 89 Liberdade, São Paulo 01509-001</p>
+            </div>
+            <div class="col ">
+              <div class="imagem-localizacao"> <img src="img/telefone.png" class="img-fluid " alt="Imagem não carregou">
+              </div>
+              <p class="mt-3">(11) 3120-2515</p>
+            </div>
+            <div class="col ">
+              <div class="imagem-localizacao"> <img src="img/email.png" class="img-fluid " alt="Imagem não carregou">
+              </div>
+              <p class="mt-3">icom@icomseguros.com.br</p>
+            </div>
+          </div>
+        </div>
+        <!--formulario-->
+        <div class="col-md-6  ">
+          <div class="card  ml-auto mr-auto" style="width:450px">
+            <div class="card-body border-sombra">
+              <h4 class="card-title">Contato</h4>
+              <h6 class="card-subtitle mb-2 text-muted">Mande uma mensagem para nós</h6>
+              <form action="https://rockcontent.com/blog/yoast-seo/" method="get">
+
+                <div class="form-group row">
+                  <label for="inputEmail3" class="col-sm-2 col-form-label"> <img src="img/nome.png" class="img-fluid "
+                      alt="Imagem não carregou"></label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" id="inputEmail3" placeholder="Nome">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label for="inputPassword3" class="col-sm-2 col-form-label"><img src="img/email2.png"
+                      class="img-fluid " alt="Imagem não carregou"></label>
+                  <div class="col-sm-10">
+                    <input type="email" class="form-control" id="inputPassword3" placeholder="E-mail">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label for="inputPassword3" class="col-sm-2 col-form-label" alt="Imagem não carregou"><img
+                      src="img/titulo-email.png" class="img-fluid " alt="Imagem não carregou"></label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" id="inputPassword3" placeholder="Titulo E-mail">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label for="inputPassword3" class="col-sm-2 col-form-label"><img src="img/mensagem.png"
+                      class="img-fluid " alt="Imagem não carregou"></label>
+                  <div class="col-sm-10">
+                    <textarea class="form-control" rows="5" id="comment" placeholder="Mensagem"></textarea>
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <div class="col-sm-10">
+                    <button type="submit" value="Submit" class="btn btn-primary">Enviar</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </section>
+
+  <form  id="form1" name="form1" method="post" action="enviarEmail.php">
+  <table  width="500" border="0" align="center" cellpadding="0" cellspacing="2">
+    <tr>
+      <td  align="right">Nome:</td>
+      <td><input  type="text" name="nome" id="nome" /></td>
+    </tr>
+    <tr>
+      <td  align="right">Assunto:</td>
+      <td><input  type="text" name="assunto" id="assunto" /></td>
+    </tr>
+    <tr>
+      <td  align="right">Mensagem:</td>
+      <td><textarea  name="mensagem" id="mensagem" cols="45" rows="5"></textarea></td>
+    </tr>
+    <tr>
+      <td  colspan="2" align="center"><input type="submit" value="Enviar" /></td>
+    </tr>
+  </table>
+</form>
+
+  <!--footer-->
+  <footer class="page-footer menu mt-5">
+    <div class=" text-center py-3">© 2019 Copyright:
+
+    </div>
+  </footer>
+
+
+
+</body>
+<script src='https://code.jquery.com/jquery-1.12.4.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.min.js'></script>
+<script src="js/index.js"></script>
+
+</html>
--- js/index.js
+++ js/index.js
@@ -1,25 +1,63 @@
 /* menu */
 function menu() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
 
-  var linkClicked = document.getElementsByClassName('nav-link');
+var linkClicked = document.getElementsByClassName('nav-link');
 var numClass = linkClicked.length;
 
 for (var i = 0; i < numClass; i++) {
-		linkClicked[i].addEventListener('click', function(){
-      var onTheMoment = document.getElementsByClassName('active');
-			onTheMoment[0].className = onTheMoment[0].className.replace(' active', '');
-			this.className += ' active';
-    }, false);
-  }
-  
+  linkClicked[i].addEventListener('click', function(){
+    var onTheMoment = document.getElementsByClassName('active');
+    onTheMoment[0].className = onTheMoment[0].className.replace(' active', '');
+    this.className += ' active';
+  }, false);
+}
 
 
+$(document).ready(function() {
+  $("#depoimentos-carousel").on("slide.bs.carousel", function(e) {
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 3;
+    var totalItems = $(".carousel-item").length;
 
-  
+    if (idx >= totalItems - (itemsPerSlide - 1)) {
+      var it = itemsPerSlide - (totalItems - idx);
+      for (var i = 0; i < it; i++) {
+        // append slides to end
+        if (e.direction == "left") {
+          $(".carousel-item")
+            .eq(i)
+            .appendTo(".carousel-inner");
+        } else {
+          $(".carousel-item")
+            .eq(0)
+            .appendTo($(this).find(".carousel-inner"));
+        }
+      }
+    }
+  });
+});
+
+
+$(document).ready(function(){
+  $("#testimonial-slider").owlCarousel({
+      items:3,
+      itemsDesktop:[1199,2],
+      itemsDesktopSmall:[979,2],
+      itemsTablet:[768,2],
+      itemsMobile:[600,1],
+      pagination:true,
+      navigation:false,
+      navigationText:["",""],
+      slideSpeed:1000,
+      autoPlay:true,
+     
+  });
+});
